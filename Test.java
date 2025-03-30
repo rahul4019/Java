57cd8ee4ede6730985dfd5c8370c7ad960485e03
rahul4019 <rahulkashyap4019@gmail.com>
2025-03-30 20:33:39 +0530

    String and Integer class

--- Test.java
+++ Test.java
@@ -1,7 +1,12 @@
 public class Test {
     public static void main(String[] args) {
-        System.out.println(args[0]);
+        // System.out.println(args[0]);
         System.out.println("Hello world!");
+
+        int a = 1;
+        System.out.println(Integer.toString(a));
+
+        String name = "Rahul Kumar";
+        System.out.println(name);
     }
 }
-
