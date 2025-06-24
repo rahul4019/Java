47736fa14e306e2016df457b63ddf32ccb2be97d
rahul4019 <rahulkashyap4019@gmail.com>
2025-06-24 07:22:29 +0530

    Calling a servlet from servlet using request dispatcher

--- servlet/DemoApp/src/AddServlet.java
+++ servlet/DemoApp/src/AddServlet.java
@@ -1,19 +1,26 @@
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class AddServlet extends HttpServlet {
-    public void service(HttpServletRequest req, HttpServletResponse res) throws IOException {
+
+    public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
         int i = Integer.parseInt(req.getParameter("num1"));
         int j = Integer.parseInt(req.getParameter("num2"));
 
         int k = i + j;
 
-        System.out.println("result is " + k);
-        PrintWriter out = res.getWriter();
-        out.println("result is " + k);
+        // passing value to other servelet
+        req.setAttribute("k", k);
+
+        // Req Dispatcher
+        RequestDispatcher rd = req.getRequestDispatcher("sq");
+        rd.forward(req, res);
+
     }
 }
