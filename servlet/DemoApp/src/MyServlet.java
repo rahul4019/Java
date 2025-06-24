cd6795d90bbd1e7d0bc5182071b97db37d709172
rahul4019 <rahulkashyap4019@gmail.com>
2025-06-24 20:48:05 +0530

    Servlet config and context

---
+++ servlet/DemoApp/src/MyServlet.java
@@ -0,0 +1,24 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class MyServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+        PrintWriter out = res.getWriter();
+        out.println("Hi ");
+
+        // ServletContext ctx = getServletContext();
+        // String str = ctx.getInitParameter("name");
+
+        ServletConfig cg = getServletConfig();
+        String str = cg.getInitParameter("name");
+        out.println(str);
+    }
+
+}
