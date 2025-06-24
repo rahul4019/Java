adb7c00c0220d58f53b250b5f88487981d19f0d9
rahul4019 <rahulkashyap4019@gmail.com>
2025-06-24 20:55:50 +0530

    servlet annotation

--- servlet/DemoApp/src/MyServlet.java
+++ servlet/DemoApp/src/MyServlet.java
@@ -4,10 +4,12 @@ import java.io.PrintWriter;
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+@WebServlet("/home")
 public class MyServlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
         PrintWriter out = res.getWriter();
